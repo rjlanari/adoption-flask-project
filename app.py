6f322e76e2db661f0766e157c9cad8a29addab8c
rjlanari <rjlanari@gmail.com>
2022-11-18 16:03:25 +0100

    Edit and Delete pages

--- app.py
+++ app.py
@@ -6,7 +6,8 @@ from models import db, Pet, app
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    pets = Pet.query.all()
+    return render_template('index.html', pets=pets) #have to pass the result of the query in the render so we can use it there
 
 @app.route('/add-pet', methods=['GET', 'POST'])
 def add_pet():
@@ -18,17 +19,52 @@ def add_pet():
                         size=request.form['size'], weight=request.form['weight'],
                         url=request.form['url'], url_tag=request.form['alt'],
                         pet_type=request.form['pet'], gender=request.form['gender'],
-                        spay=request.form['spay'], house_trained=request.form['house_trained'],
-                        desctription=request.form['desctription'])
+                        spay=request.form['spay'], house_trained=request.form['housetrained'],
+                        description=request.form['description'])
         '''creates the Python object'''
         db.session.add(new_pet)
         db.session.commit()
         return redirect(url_for('index'))
     return render_template('addpet.html')
 
-@app.route('/pet')
-def pet():
-    return render_template('pet.html')
+@app.route('/pet/<id>') #inside () with <> to pass info from one page to another
+def pet(id):
+    pet = Pet.query.get_or_404(id)
+    return render_template('pet.html', pet=pet)
+
+
+@app.route('/edit/<id>', methods=['GET', 'POST'])
+def edit_pet(id):
+    pet = Pet.query.get_or_404(id)
+    if request.form:
+        pet.name = request.form['name']
+        pet.age = request.form['age']
+        pet.breed = request.form['breed']
+        pet.color = request.form['color']
+        pet.size = request.form['size']
+        pet.weight = request.form['weight']
+        pet.url = request.form['url']
+        pet.url_tag = request.form['alt']
+        pet.pet_type = request.form['pet']
+        pet.gender = request.form['gender']
+        pet.spay = request.form['spay']
+        pet.weight = request.form['housetrained']
+        pet.description = request.form['description']
+        db.session.commit()
+        return redirect(url_for('index'))
+
+    return render_template('editpet.html', pet=pet)
+
+@app.route('/delet_pet/<id>')
+def delete_pet(id):
+    pet = Pet.query.get_or_404(id)
+    db.session.delete(pet)
+    db.session.commit()
+    return redirect(url_for('index'))
+
+@app.errorhandler(404)
+def not_found(error):
+    return render_template('404.html', msg=error), 404
 
 
 if __name__ == '__main__':
